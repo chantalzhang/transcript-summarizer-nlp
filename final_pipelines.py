cc2200f707d53028d06988d7956a8f9cd980737d
chantalzhang <147449578+chantalzhang@users.noreply.github.com>
2024-12-17 18:10:44 -0500

    added more filler words and phrases to ignore in the noise removal step

--- final_pipelines.py
+++ final_pipelines.py
@@ -10,49 +10,58 @@ class PreProcessor:
         self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased") # no need to use api key for just token limit counting 
 
         self.filler_words = [
-            # Hesitation markers and phrases that he uses a lot
-            "uh", "um", "yeah", "okay", "alright", "so", "sorry",
-            "like", "you know", "i mean", "kind of", "sort of",
-            "basically", "actually", "pretty much",
-            "hmm", "err", "well", "let's see", "let me think",
-            
-            # Lecture-specific fillers
-            "moving on", "next", "now", "right", "anyway",
-            "as i said", "as i mentioned", "like i said"
-        ]
-        
-        self.ignore_phrases = [
-            # Lecture openings/transitions
-            "good morning", "let me share my screen", "hello class",
-            "hey everyone", "today we will", "welcome to",
-            "let's get started", "can everyone see my screen",
-            "before we begin", "let's move forward",
+        "uh", "um", "yeah", "okay", "alright", "so", "sorry", "like", "you know", "i mean",
+        "kind of", "sort of", "basically", "actually", "pretty much", "hmm", "err", "well",
+        "let's see", "let me think", "well", "let's try", "I guess", "moving on", "next",
+        "now", "right", "anyway", "as i said", "as i mentioned", "like i said", "let's look at",
+        "let me show you", "let's say", "for example", "in other words", "in this case", 
+        "you can see", "you might have", "you could have", "let's go through", "let's talk about",
+        "we can see", "we'll see", "what I mean is", "the idea is", "the point is", "the thing is",
+        "i think", "maybe", "probably", "possibly", "perhaps", "supposedly", "apparently",
+        "presumably", "essentially", "generally speaking", "more or less", "roughly speaking",
+        "in a sense", "in some sense", "in some way"
+    ]
 
-            # Class management unrelated to content
-            "any questions so far", "does that make sense",
-            "raise your hand if", "let me know if",
-            "is everyone following", "can everyone hear me",
-            "we have time for", "in the remaining time",
-            "we'll cover this next time", "in our next lecture",
-            
-            # Recap phrases
-            "as we discussed last time", "remember from last class",
-            "just to recap", "to summarize what we covered"
-        ]
-        
+        self.ignore_phrases = [
+        "good morning", "let me share my screen", "hello class", "hey everyone", "today we will",
+        "welcome to", "let's get started", "can everyone see my screen", "before we begin",
+        "let's move forward", "first of all", "hello,? how are we all doing", "all,? welcome back",
+        "today it's me", "OK,? today we're going to start", "before we begin", "let's get started",
+        "any questions so far", "does that make sense", "are there any questions", "remember from last class",
+        "the readings for the course", "lecture slides have already been posted", "make sure that you have access",
+        "office hours have been posted", "raise your hand if", "let me know if", "is everyone following",
+        "can everyone hear me", "we have time for", "in the remaining time", "we'll cover this next time",
+        "in our next lecture", "raise your hand if", "we'll cover this next time", "as we discussed last time",
+        "remember from last class", "just to recap", "to summarize what we covered", 
+        "let me turn my phone off to do not disturb", "i'll give you a minute", "take a few minutes",
+        "let's take a break", "we'll come back to this", "i'll wait for everyone", "does anyone want to share",
+        "anyone want to try", "raise your hands if", "type in the chat", "in breakout rooms", 
+        "for this exercise", "work with your neighbor", "discuss with your partner", "let me know when you're done",
+        "i'll give you some time", "let me switch screens", "can you see my cursor", "let me make this bigger",
+        "i'll zoom in", "i'll zoom out", "let me scroll down", "let me scroll up", "let me go back",
+        "going back to", "moving to the next slide", "on this slide", "as you can see here",
+        "looking at this example", "if we look at", "if you look at", "coming back to", "to summarize", 
+        "in conclusion", "wrapping up", "that brings us to", "moving forward", "let's continue with",
+        "getting back to", "on that note", "speaking of which", "by the way", "as an aside", 
+        "quick reminder", "quick note", "one more thing"
+    ]
+
+
+        self.filler_pattern = re.compile(rf"\b({'|'.join(self.filler_words)})\b", re.IGNORECASE)
         self.ignore_pattern = re.compile(rf"\b({'|'.join(self.ignore_phrases)})\b", re.IGNORECASE)
 
     def noise_removal(self, text):
-        """ Removes filler words, repeated words/phrases, and ignored phrases. """
-        filler_pattern = re.compile(rf"\b({'|'.join(self.filler_words)})\b", re.IGNORECASE)
-        text = filler_pattern.sub("", text)  # Remove filler words
+        """ Removes filler words, repeated wordxs/phrases, and ignored phrases. """
+        text = self.filler_pattern.sub("", text)  # Remove filler words
         text = re.sub(r"\b(\w+)( \1\b)+", r"\1", text)  # Remove repeated words
         text = self.ignore_pattern.sub("", text)  # Remove ignored phrases
         return text
 
     def split_by_sentences(self, text):
         """ Splits text into sentences. """
-        return sent_tokenize(text)
+        sentences = sent_tokenize(text)
+        filtered_sentences = [sentence for sentence in sentences if len(sentence.split()) > 4]
+        return filtered_sentences
 
     def split_by_fixed_sentences(self, sentences, chunk_size):
         """ Split sentences into fixed-size chunks. """
